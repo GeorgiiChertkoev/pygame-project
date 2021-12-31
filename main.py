aa826d100261aee5ef3cefd99ba9f3da8c534fc9
GeorgiiChertkoev <geochert13028@gmail.com>
2022-01-01 02:19:10 +0300

    plus some pics

--- main.py
+++ main.py
@@ -14,11 +14,28 @@ class Sprite(pygame.sprite.Sprite):
 
 
 class Tile(Sprite):
+    group = pygame.sprite.Group()
+    bg = pygame.image.load('data/bg_tile2.png')
+    wall = pygame.Surface((62, 62))
+    wall.fill(0)
+
     """
     есть показатель верхняя ли это стенка
     """
-    pass
 
+
+    def update(self):
+        pass
+
+
+class Floor(Tile):
+    value = 0
+    image = Tile.bg
+    # group = pygame.sprite.Group()
+
+class Wall(Tile):
+    value = 1
+    image = Tile.wall
 
 class Hero(Sprite):
     image = pygame.image.load('data/hero_1.png')
@@ -26,17 +43,33 @@ class Hero(Sprite):
 
     def update(self):
         # if not pygame.sprite.spritecollideany(self, Platform.group):
-            # self.rect.y += 1
+        # self.rect.y += 1
         keys = pygame.key.get_pressed()
-        self.rect.x += (keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * 3
-        self.rect.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP]) * 3
+
+        self.rect.x += (keys[pygame.K_d] - keys[pygame.K_a] +
+                        keys[pygame.K_RIGHT] - keys[pygame.K_LEFT]) * 3
+        self.rect.y += (keys[pygame.K_DOWN] - keys[pygame.K_UP] +
+                        keys[pygame.K_s] - keys[pygame.K_w]) * 3
 
 
-sc_center = screen.get_rect().center
+sc_center = pygame.math.Vector2(screen.get_rect().center)
 clock = pygame.time.Clock()
 
 
 Hero(sc_center)
+
+# рисуем карту
+for row in range(0, h + 60, 60):
+    for col in range(0, w + 60, 60):
+        if (col / 60) % 2:
+            Floor((row, col))
+        else:
+            Wall((row, col))
+
+# def load_map(filename=''):
+
+
+
 
 running = True
 start = 0
@@ -46,6 +79,10 @@ while running:
             running = False
 
     screen.fill(0)
+
+    Floor.group.draw(screen)
+    Floor.group.update()
+
     Hero.group.draw(screen)
     Hero.group.update()
 
