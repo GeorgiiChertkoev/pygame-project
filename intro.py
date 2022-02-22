52a3942d4dd2daee882aa8e36804f01c2ad0a0d0
GeorgiiChertkoev <geochert13028@gmail.com>
2022-02-22 22:32:35 +0300

    1 level playable, endless

---
+++ intro.py
@@ -0,0 +1,73 @@
+import pygame
+import pygame_gui
+import sys
+
+size = w, h = 800, 600
+FPS = 60
+pygame.init()
+
+screen = pygame.display.set_mode(size)
+manager = pygame_gui.UIManager(size)
+bg = pygame.Surface(size)
+bg.fill('red')
+
+
+def start_window():
+    level1_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((600, 50), (100, 50)),
+        text='1',
+        manager=manager)
+
+    level2_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((600, 150), (100, 50)),
+        text='2',
+        manager=manager)
+
+    level3_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((600, 250), (100, 50)),
+        text='3',
+        manager=manager)
+
+    level4_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((600, 350), (100, 50)),
+        text='4',
+        manager=manager)
+
+    level5_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((600, 450), (100, 50)),
+        text='5',
+        manager=manager)
+
+
+def intro():
+    start_window()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                return event.ui_element.text
+            manager.process_events(event)
+
+        pygame.transform.scale(pygame.image.load('data/fon.jpg').convert(),
+                               size, screen)
+        manager.update(clock.tick(FPS) / 1000)
+        manager.draw_ui(screen)
+        pygame.display.update()
+
+
+clock = pygame.time.Clock()
+print(intro())
+run = True
+# run = 0
+while run:
+    fps = clock.tick(60) / 1000
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    screen.blit(bg, (0, 0))
+    pygame.display.update()
+
+
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pygame
+import pygame_gui
 
 
 w, h = size = 600, 520
 tw, th = 40, 40
+FPS = 60
 
 
 pygame.init()
 screen = pygame.display.set_mode(size)
+manager = pygame_gui.UIManager(size)
+
+heart = pygame.image.load("data/heart.png").convert_alpha()
+empty_heart = pygame.image.load("data/empty_heart1.png").convert_alpha()
 
 
 class Tile(pygame.sprite.Sprite):
@@ -34,12 +40,11 @@ class Hero(pygame.sprite.Sprite):
 
     def __init__(self, pos):
         super().__init__(self.group)
+        self.hp = 3
+        self.damaged = 0
         self.rect = self.image.get_rect(topleft=pos)
 
     def update(self):
-        # if not pygame.sprite.spritecollideany(self, Platform.group):
-        # self.rect.y += 1
-
         keys = pygame.key.get_pressed()
 
         delta = ((keys[pygame.K_d] - keys[pygame.K_a] +
@@ -50,45 +55,136 @@ class Hero(pygame.sprite.Sprite):
         old_rect = self.rect
         self.rect = self.rect.move(delta)
         if (pygame.sprite.spritecollideany(self, Tile.walls)
-            or not screen.get_rect().contains(self.rect)):
+                or not screen.get_rect().contains(self.rect)):
             self.rect = old_rect
+        if pygame.sprite.spritecollideany(
+                self, Enemy.group) and not self.damaged:
+            self.hp -= 1
+            print(self.hp)
+            self.damaged = 60
+        self.damaged = max(0, self.damaged - 1)
+        if self.hp == 0:
+            end_screen(1)
 
 
 class Enemy(pygame.sprite.Sprite):
     group = pygame.sprite.Group()
-    image = pygame.image.load('data/enemy.png')
+    image = pygame.image.load('data/enemy1.png')
 
-    def __init__(self, pos, move_style=(0, 0), speed=5):
+    def __init__(self, pos, move_style=(0, 0), speed=0):
         # move_style = direction
         super().__init__(self.group)
         self.tick = 0
-        self.delta = pygame.math.Vector2(move_style) * 5
+        self.delta = pygame.math.Vector2(move_style) * speed
+        self.image = Enemy.image
         self.rect = self.image.get_rect(topleft=pos)
 
     def update(self):
         old_rect = self.rect
         self.rect = self.rect.move(self.delta)
         if (pygame.sprite.spritecollideany(self, Tile.walls)
-            or not screen.get_rect().contains(self.rect)):
+                or not screen.get_rect().contains(self.rect)):
             self.delta *= -1
             self.rect = old_rect
 
 
+class Hearts(pygame.sprite.Sprite):
+    group = pygame.sprite.GroupSingle()
+
+    def __init__(self, hero):
+        super().__init__(self.group)
+        self.hero = hero
+        self.old_hp = 4
+        self.update()
+        self.rect = self.image.get_rect()
+
+    def update(self):
+        if self.old_hp == self.hero.hp:
+            return
+        surf = pygame.Surface((120, 40))
+        surf.fill((4, 0, 0))
+        surf.set_colorkey((4, 0, 0, 255))
+        for i in range(0, 3):
+            if self.hero.hp >= i + 1:
+                surf.blit(heart, (i * 40, 0))
+            else:
+                surf.blit(empty_heart, (i * 40, 0))
+
+        # surf.set_alpha(255)
+        self.image = surf
+        self.old_hp == self.hero.hp
 
 
-clock = pygame.time.Clock()
+def start_window():
+    level1_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((500, 50), (100, 50)),
+        text='1',
+        manager=manager)
+
+    level2_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((500, 150), (100, 50)),
+        text='2',
+        manager=manager)
+
+    level3_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((500, 250), (100, 50)),
+        text='3',
+        manager=manager)
+
+    level4_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((500, 350), (100, 50)),
+        text='4',
+        manager=manager)
+
+    level5_but = pygame_gui.elements.UIButton(
+        relative_rect=pygame.Rect((500, 450), (100, 50)),
+        text='5',
+        manager=manager)
 
 
-# a = Hero(sc_center)
-# print(a.rect)
+def intro():
+    start_window()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
 
-# рисуем карту
-# for row in range(0, h + 60, 60):
-#     for col in range(0, w + 60, 60):
-#         if (col / 60) % 2:
-#             Tile((row, col))
-#         else:
-#             Tile((row, col), 1)
+            if event.type == pygame_gui.UI_BUTTON_PRESSED:
+                return event.ui_element.text
+                break
+            manager.process_events(event)
+
+        pygame.transform.scale(pygame.image.load('data/fon.jpg').convert(),
+                               size, screen)
+        manager.update(clock.tick(FPS))
+        manager.draw_ui(screen)
+        pygame.display.update()
+
+
+def blackout():
+    transparent = pygame.Surface(size)
+    transparent.set_alpha(60)
+    # Плавный переход
+    for i in range(7):
+        screen.blit(transparent, (0, 0))
+        pygame.display.flip()
+        pygame.time.wait(30)
+
+
+def end_screen(level):
+    font = pygame.font.Font('data/fonts/casual.ttf', 20)
+    text = font.render('Вы проиграли', 0, 'red', 0)
+    t_rect = text.get_rect()
+    blackout()
+    screen.blit(text, (w // 2 - t_rect.w // 2, 50))
+    pygame.display.update()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            if event.type in (pygame.KEYDOWN,
+                              pygame.MOUSEBUTTONDOWN):
+                draw_map(intro())
 
 
 def load_map(filename):
@@ -97,51 +193,72 @@ def load_map(filename):
     return [x.rstrip().ljust(maxlen, '.') for x in lines], maxlen, len(lines)
 
 
-def draw_map(filename='1.map'):
-    data, w, h = load_map(filename)
-    surface = pygame.Surface((w * tw, h * th))
-    dmap = {'.': 0, '#': 1, '1': 2, '@': 3, '-':}
+def draw_map(level=1):
+    data, w, h = load_map(f'data/level{level}/map.txt')
+    dmap = {'.': 0, '#': 1, '1': 2,
+            '@': 3, '-': (1, 0),
+            '|': (0, 1)}
+    cleaner()
     for y in range(len(data)):
         for x in range(len(data[0])):
             value = dmap[data[y][x]]
+            pos = (x * tw, y * th)
             if value == 3:
                 # герой
-                hero = Hero((x * tw, y * th))
+                hero = Hero(pos)
                 value = 0
-            Tile((x * tw, y * th), 1, value)
-    Tile.floor.draw(surface)
-    Tile.walls.draw(surface)
-    return surface, hero
-
-
-bg, hero = draw_map()
-scene = bg.copy()
-
-area = screen.get_rect(center=hero.rect.center)
-Hero.group.draw(scene)
-screen.blit(scene, (0, 0), area)
-pygame.display.update()
-
-
-running = True
-start = 0
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.fill(0)
-
-    Tile.walls.update()
-    Tile.walls.draw(screen)
-    Tile.floor.update()
+            if isinstance(value, tuple):
+                Enemy(pos, move_style=value, speed=4)
+                value = 0
+            Tile(pos, 1, value)
     Tile.floor.draw(screen)
-    Tile.escape.update()
-    Tile.escape.draw(screen)
+    Tile.walls.draw(screen)
+    Enemy.group.draw(screen)
+
+    updater(hero)
 
 
-    Hero.group.draw(screen)
-    Hero.group.update()
+def updater(hero):
+    Hearts(hero)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    pygame.display.update()
-    clock.tick(60)
+        # screen.fill(0)
+
+        Tile.walls.update()
+        Tile.walls.draw(screen)
+        Tile.floor.update()
+        Tile.floor.draw(screen)
+        Tile.escape.update()
+        Tile.escape.draw(screen)
+
+        Hero.group.draw(screen)
+        Hero.group.update()
+
+        Enemy.group.draw(screen)
+        Enemy.group.update()
+
+        Hearts.group.update()
+        Hearts.group.draw(screen)
+
+        pygame.display.update()
+        clock.tick(FPS)
+
+def cleaner():
+    Tile.walls.empty()
+    Tile.floor.empty()
+    Tile.escape.empty()
+    Hero.group.empty()
+    Enemy.group.empty()
+    Hearts.group.empty()
+
+pygame.display.update()
+clock = pygame.time.Clock()
+
+
+# running = 0
+
+draw_map(intro())
